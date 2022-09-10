c3da3180aff5c56a3ff040f2370906ca19033d07
Beatriz Niemeyer <bianiemeyer@beatrizs-air.mynetworksettings.com>
2022-09-10 18:32:45 -0400

    another list exercise

--- algorithms.py
+++ algorithms.py
@@ -149,7 +149,34 @@ def sum_closest_target(nums, target):
 print(sum_closest_target([4, 0, 5, -5, 3, 3, 0, -4, -5], -2))
 
 
+# 121. Best Time to Buy and Sell Stock
+
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+# Example 1:
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+
 def stock(prices):
 
-    for i in range(len(prices) - 1):
-        if prices[i] == prices[i + 1] and i != 0:
+    buy, sell = 0, 1
+    profit = 0
+
+    while sell < len(prices):
+        if prices[buy] < prices[sell]:
+            earnings = sell - buy
+            profit = max(profit, earnings)
+
+        else:
+            buy = sell
+
+        sell += 1
